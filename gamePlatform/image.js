320738bbe4c7fb8470f0b0b7866b9c94cd83c552
Jakub Kliś <jakubklis2201@gmail.com>
2023-05-18 17:11:28 +0200

    Added array with images

--- gamePlatform/image.js
+++ gamePlatform/image.js
@@ -17,4 +17,116 @@ export const allMemoryImages = [
     src: require("./assets/images/BigBen.png"),
     matched: false,
   },
+  {
+    src: require("./assets/images/Bull.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Butterfly.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Chest.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/CruiseShip.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/EiffelTower.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Hydra.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Iceberg.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Lion.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Maracas.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Monkey.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Octopus.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Pharaoh.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Pterodactyl.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Rhinoceros.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Rooster.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Seahorse.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Shark.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Spider.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/StatueOfLiberty.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Stegosaurus.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Tent.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/TribalSymbols.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/VenetianMask.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Volcano.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Wasp.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Whale.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/Yak.png"),
+    matched: false,
+  },
+  {
+    src: require("./assets/images/YinYang.png"),
+    matched: false,
+  },
 ];
