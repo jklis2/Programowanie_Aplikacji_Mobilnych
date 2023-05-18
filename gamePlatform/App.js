d34e373134559a3eb99c4e608f5f84a99a001ca6
Jakub Kliś <jakubklis2201@gmail.com>
2023-05-18 13:04:36 +0200

    Main screen

--- gamePlatform/App.js
+++ gamePlatform/App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const image = require("./assets/rainbow-vortex.svg");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={image} resizeMode="cover" style={styles.background}>
+        <Text style={styles.appTitle}>Gra pamięciowa</Text>
+        <TouchableOpacity style={styles.buttons}>
+          <Text style={styles.buttonsText}>Zacznij grę</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttons}>
+          <Text style={styles.buttonsText}>Ustawienia</Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +23,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  appTitle: {
+    color: '#fff',
+    fontSize: 48,
+  },
+  buttons: {
+    backgroundColor: '#f4a44e',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    width: 300,
+  },
+  buttonsText: {
+    color: '#fff',
+    fontSize: 24,
+    textAlign: 'center',
   },
 });
